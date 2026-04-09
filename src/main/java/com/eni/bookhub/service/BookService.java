ae31e07cf7687ac5a45a22c7adfe73fb8cec2d39
Clement PRAUD <clement.praud2025@campus-eni.fr>
2026-04-09 16:55:18 +0200

    CP - Create book repository, service, service implementation and controller to handle getBook, getBookById and addBook routes

---
+++ src/main/java/com/eni/bookhub/service/BookService.java
@@ -0,0 +1,12 @@
+package com.eni.bookhub.service;
+
+import com.eni.bookhub.BO.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookService {
+    List<Book> getBooks();
+    Optional<Book> getBookById(Integer bookId);
+    void addBook(Book book);
+}
