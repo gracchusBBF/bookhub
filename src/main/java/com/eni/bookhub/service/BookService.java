1037be589549bcafc5f2197d30d7881bb1658bf9
Clement PRAUD <clement.praud2025@campus-eni.fr>
2026-04-10 15:40:38 +0200

    CP - create searchBook process to retrieve book with author first and/or last name, isbn or title; add pagination on getAllBooks and searchBooks endpoints and create a starter script that write 20 lines in book entity at serveur start

--- src/main/java/com/eni/bookhub/service/BookService.java
+++ src/main/java/com/eni/bookhub/service/BookService.java
@@ -6,9 +6,14 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BookService {
-    List<Book> getBooks();
+    List<Book> getBooks(int pageNum);
     Optional<Book> getBookById(Integer bookId);
     void addBook(Book book);
+
+    Optional<Book> getBookByTitle(String title);
+
     Book updateBook(Book book);
     void deleteBook(Integer bookId);
+
+    Optional<List<Book>> searchBooks(int pageNum, String query);
 }
