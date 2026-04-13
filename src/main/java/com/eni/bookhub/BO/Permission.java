fb3d58f9829798edf65cf7eda91d1157a610ff54
Clement PRAUD <clement.praud2025@campus-eni.fr>
2026-04-13 11:01:10 +0200

    CP - update permission entity

--- src/main/java/com/eni/bookhub/BO/Permission.java
+++ src/main/java/com/eni/bookhub/BO/Permission.java
@@ -1,6 +1,9 @@
 package com.eni.bookhub.BO;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.HashSet;
@@ -13,9 +16,13 @@ import java.util.Set;
 public class Permission {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "permission_id")
-    private int id;
+    private Integer id;
 
+    @NotNull
+    @NotBlank
+    @Size(max = 100)
     @Column(name = "permission_name", nullable = false, unique = true, length = 100)
     private String permissionName;
 }
